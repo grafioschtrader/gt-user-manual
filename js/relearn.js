9fb6cf2c711e3c7c0af9523aba5e0fd97f085ef9
hugograf <hugograf@users.noreply.github.com>
2021-08-04 19:46:30 +0000

    deploy: 1fe00ff64a68b8e3b667894e0796e62a54d08903

--- js/relearn.js
+++ js/relearn.js
@@ -47,6 +47,132 @@ function fallbackMessage(action) {
     return actionMsg;
 }
 
+function switchTab(tabGroup, tabId) {
+    allTabItems = jQuery("[data-tab-group='"+tabGroup+"']");
+    targetTabItems = jQuery("[data-tab-group='"+tabGroup+"'][data-tab-item='"+tabId+"']");
+
+    // if event is undefined then switchTab was called from restoreTabSelection
+    // so it's not a button event and we don't need to safe the selction or
+    // prevent page jump
+    var isButtonEvent = event != undefined;
+
+    if(isButtonEvent){
+      // save button position relative to viewport
+      var yposButton = event.target.getBoundingClientRect().top;
+    }
+
+    allTabItems.removeClass("active");
+    targetTabItems.addClass("active");
+
+    if(isButtonEvent){
+      // reset screen to the same position relative to clicked button to prevent page jump
+      var yposButtonDiff = event.target.getBoundingClientRect().top - yposButton;
+      window.scrollTo(window.scrollX, window.scrollY+yposButtonDiff);
+
+      // Store the selection to make it persistent
+      if(window.localStorage){
+          var selectionsJSON = window.localStorage.getItem("tabSelections");
+          if(selectionsJSON){
+            var tabSelections = JSON.parse(selectionsJSON);
+          }else{
+            var tabSelections = {};
+          }
+          tabSelections[tabGroup] = tabId;
+          window.localStorage.setItem("tabSelections", JSON.stringify(tabSelections));
+      }
+    }
+}
+
+function restoreTabSelections() {
+    if(window.localStorage){
+        var selectionsJSON = window.localStorage.getItem("tabSelections");
+        if(selectionsJSON){
+          var tabSelections = JSON.parse(selectionsJSON);
+        }else{
+          var tabSelections = {};
+        }
+        Object.keys(tabSelections).forEach(function(tabGroup) {
+          var tabItem = tabSelections[tabGroup];
+          switchTab(tabGroup, tabItem);
+        });
+    }
+}
+
+function initMermaid() {
+    $('code.language-mermaid').each(function(index, element) {
+        var content = $(element).html().replace(/&amp;/g, '&');
+        $(element).parent().replaceWith('<div class="mermaid" align="center">' + content + '</div>');
+    });
+
+    if (mermaid) {
+        mermaid.mermaidAPI.initialize( Object.assign( {}, mermaid.mermaidAPI.getSiteConfig(), { startOnLoad: true } ) );
+        mermaid.contentLoaded();
+        $(".mermaid svg").svgPanZoom({})
+    }
+}
+
+// Get Parameters from some url
+var getUrlParameter = function getUrlParameter(sPageURL) {
+    var url = sPageURL.split('?');
+    var obj = {};
+    if (url.length == 2) {
+      var sURLVariables = url[1].split('&'),
+          sParameterName,
+          i;
+      for (i = 0; i < sURLVariables.length; i++) {
+          sParameterName = sURLVariables[i].split('=');
+          obj[sParameterName[0]] = sParameterName[1];
+      }
+    }
+    return obj;
+};
+
+// Execute actions on images generated from Markdown pages
+var images = $("div#body-inner img").not(".inline");
+// Wrap image inside a featherlight (to get a full size view in a popup)
+images.wrap(function(){
+    var image =$(this);
+    var o = getUrlParameter(image[0].src);
+    var f = o['featherlight'];
+    // IF featherlight is false, do not use feather light
+    if (f != 'false') {
+        if (!image.parent("a").length) {
+            return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+        }
+    }
+});
+
+// Change styles, depending on parameters set to the image
+images.each(function(index){
+    var image = $(this)
+    var o = getUrlParameter(image[0].src);
+    if (typeof o !== "undefined") {
+        var h = o["height"];
+        var w = o["width"];
+        var c = o["classes"];
+        image.css("width", function() {
+            if (typeof w !== "undefined") {
+                return w;
+            } else {
+                return "auto";
+            }
+        });
+        image.css("height", function() {
+            if (typeof h !== "undefined") {
+                return h;
+            } else {
+                return "auto";
+            }
+        });
+        if (typeof c !== "undefined") {
+            var classes = c.split(',');
+            for (i = 0; i < classes.length; i++) {
+                image.addClass(classes[i]);
+            }
+        }
+    }
+});
+
 // for the window resize
 $(window).resize(function() {
     setMenuHeight();
@@ -82,7 +208,10 @@ $(window).resize(function() {
 })(jQuery, 'smartresize');
 
 
-jQuery(document).ready(function() {
+jQuery(function() {
+    restoreTabSelections();
+    initMermaid();
+
     jQuery('#sidebar .category-icon').on('click', function() {
         $( this ).toggleClass("fa-angle-down fa-angle-right") ;
         $( this ).parent().parent().children('ul').toggle() ;
@@ -173,6 +302,8 @@ jQuery(document).ready(function() {
         }
     }
 
+    $(".highlightable").highlight(sessionStorage.getItem('search-value'), { element: 'mark' });
+
     // clipboard
     var clipInit = false;
     $('code').each(function() {
@@ -206,6 +337,7 @@ jQuery(document).ready(function() {
                 clipInit = true;
             }
 
+            code.addClass('copy-to-clipboard-inline');
             code.after('<span class="copy-to-clipboard" title="Copy to clipboard" />');
             code.next('.copy-to-clipboard').on('mouseleave', function() {
                 $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
@@ -229,7 +361,7 @@ jQuery(document).ready(function() {
              e.stopPropagation();
          }
      });
-    
+
     jQuery(document).keydown(function(e) {
       // prev links - left arrow key
       if(e.which == '37') {
@@ -264,7 +396,7 @@ jQuery(document).ready(function() {
         });
     }
 
-    /** 
+    /**
     * Fix anchor scrolling that hides behind top nav bar
     * Courtesy of https://stackoverflow.com/a/13067009/28106
     *
@@ -346,31 +478,46 @@ jQuery(document).ready(function() {
 
         $(document).ready($.proxy(anchorScrolls, 'init'));
     })(window.document, window.history, window.location);
-    
-});
 
-jQuery(window).on('load', function() {
-    // store this page in session
+    // Stick the top to the top of the screen when    scrolling
+    jQuery("#top-bar").sticky({topSpacing:0, zIndex: 1000});
+
+    // Add link button for every
+    var text, clip = new ClipboardJS('.anchor');
+    $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function(index, html){
+        var element = $(this);
+        var url = encodeURI(document.location.origin + document.location.pathname);
+        var link = url + "#"+element[0].id;
+        return " <span class='anchor' data-clipboard-text='"+link+"'>" +
+            "<i class='fas fa-link fa-lg'></i>" +
+            "</span>"
+        ;
+    });
+
+    $(".anchor").on('mouseleave', function(e) {
+        $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
+    });
+
+    clip.on('success', function(e) {
+            e.clearSelection();
+            $(e.trigger).attr('aria-label', 'Link copied to clipboard!').addClass('tooltipped tooltipped-s');
+    });
+
+    $('a[rel="lightbox"]').featherlight({
+        root: 'section#body'
+    });
+
     sessionStorage.setItem(jQuery('body').data('url'), 1);
 
     // loop through the sessionStorage and see if something should be marked as visited
     for (var url in sessionStorage) {
         if (sessionStorage.getItem(url) == 1) jQuery('[data-nav-id="' + url + '"]').addClass('visited');
     }
-
-
-    $(".highlightable").highlight(sessionStorage.getItem('search-value'), { element: 'mark' });
-});
-
-$(function() {
-    $('a[rel="lightbox"]').featherlight({
-        root: 'section#body'
-    });
 });
 
 jQuery.extend({
     highlight: function(node, re, nodeName, className) {
-        if (node.nodeType === 3) {
+        if (node.nodeType === 3 && node.parentElement.namespaceURI == 'http://www.w3.org/1999/xhtml') { // text nodes
             var match = node.data.match(re);
             if (match) {
                 var highlight = document.createElement(nodeName || 'span');
